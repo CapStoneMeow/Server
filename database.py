f6ac49c8ba22f496779234d4e6e094ad43a02020
wxxnxx <wanx224@gmail.com>
2025-05-03 23:36:23 +0900

    경로 수정

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
